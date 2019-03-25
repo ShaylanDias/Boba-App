1d53ec28c6769ab4fedcbcfe752496a6dde81ded
ShaylanDias <shaylandias@gmail.com>
2019-03-24 21:40:16 -0700

    Sync

--- BobaApp/LocationViewViewController.swift
+++ BobaApp/LocationViewViewController.swift
@@ -153,8 +153,8 @@ class LocationViewViewController: UIViewController, CLLocationManagerDelegate, M
                 
                 if var region = self?.mapView.region {
                     region.center = location.coordinate
-                    region.span.longitudeDelta /= 270.0
-                    region.span.latitudeDelta /= 270.0
+                    region.span.longitudeDelta /= 180
+                    region.span.latitudeDelta /= 180
                     self?.mapView.setRegion(region, animated: true)
                     self?.mapView.addAnnotation(mark)
                 }
